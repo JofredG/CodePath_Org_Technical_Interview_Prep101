127c522f8f8a3f424c0a13b62c1ae0bb5ec83f8b
Jofred <71094875+JofredG@users.noreply.github.com>
2024-09-21 12:09:24 -0500

    Session 1 practice + UMPIRE Introduction

---
+++ session1version1.py
@@ -0,0 +1,47 @@
+# Problem Set Version 1
+# Problem 1: Hello World!
+def hello_world():
+    print("Hello world!")
+
+hello_world()
+
+# Problem 2: Today's Mood
+def todays_mood():
+    mood = "👻";
+    print("Today's mood: " + mood)
+    
+todays_mood()
+
+# Problem 3: Lunch Menu
+def print_menu(menu):
+    print("Lunch today is: " + menu)
+
+print_menu("🌮")
+
+# Problem 4: Sum of Two Integers
+def sum(a, b):
+    return a + b
+
+print(sum(5, 10))
+
+#Problem 5: Product of Two Integers
+
+def product(a, b):
+    return a * b
+
+print(product(5, 10))
+
+# Problem 6: Classify Age
+def classifyAge(age):
+    if age < 18:
+        return "child"
+    else:
+        return "adult"
+
+print(classifyAge(4))
+print(classifyAge(49))
+
+# Problem 7: What time is it?
+def whatTimeIsIt():
+    #pass
+    #if time == 2:
